a985012364a416ee558b76117128efbbca311e9e
Luciano Fantone <luciano@flybondi.com>
2018-08-22 17:48:42 -0300

    fix: check for process.release existance

--- src/utils.js
+++ src/utils.js
@@ -13,6 +13,6 @@ export const isPromise: (value: any) => boolean = is(Promise);
 export const isError: (value: any) => boolean = is(Error);
 
 export const isServerSide: () => boolean = () =>
-  typeof process !== 'undefined' && process.release.name === 'node';
+  typeof process !== 'undefined' && process.release && process.release.name === 'node';
 
 export const isClientSide: () => boolean = complement(isServerSide);
