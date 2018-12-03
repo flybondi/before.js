dc9a79e11b57dbb81b7dc211ca1781fcc3cff9ec
Luciano Fantone <lfantone@users.noreply.github.com>
2018-12-03 09:37:23 -0300

    fix: add missing export Error component

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // @flow strict;
 
 export { default as Before } from './src/Before.component';
+export { default as Error } from './src/Error.component';
 export * from './src/Async.component';
 export * from './src/Document.component';
 export * from './src/ensureReady';
