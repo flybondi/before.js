cf65f5bdf4e4198bd2a31afa288fb52f8f8dad71
Luciano Fantone <luciano@flybondi.com>
2019-01-26 21:03:33 -0300

    test: workaround to use React.memo

--- src/Before.test.js
+++ src/Before.test.js
@@ -7,6 +7,12 @@ import Before from './Before.component';
 import { StaticRouter, Switch } from 'react-router-dom';
 import { mount } from 'enzyme';
 
+// NOTE(lf): work around until React.memo is fully supported by Jest (https://github.com/airbnb/enzyme/issues/1875)
+jest.mock('react', () => {
+  const r = jest.requireActual('react');
+  return { ...r, memo: x => x };
+});
+
 test('the rendered component should have a Route component', () => {
   const DummyComponent = () => <span>Hi there!</span>;
   const context = {};
@@ -30,6 +36,7 @@ test('the rendered component should have a Route component', () => {
   const route = wrapper.find(Switch).childAt(0);
   expect(wrapper.find(Switch).props().location).toHaveProperty('pathname', '/');
   expect(wrapper.find(Switch).children()).toHaveLength(1);
+  console.log(route, wrapper, 123);
   expect(route.key()).toEqual('route--0');
   expect(route.props().path).toEqual('/');
   expect(route.props().exact).toBeTruthy();
