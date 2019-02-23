c0fc449c2a33f3c1ca6fc3b29b3d524c33fe9117
Luciano Fantone <luciano@flybondi.com>
2019-02-23 14:32:49 -0300

    refactor: move component to be a function

--- src/Document.component.jsx
+++ src/Document.component.jsx
@@ -6,7 +6,7 @@ import type {
   Extractor,
   DataType
 } from 'Document.component';
-import React, { PureComponent } from 'react';
+import React, { Fragment } from 'react';
 import { F, identity, path } from 'ramda';
 import Error from './Error.component';
 import serialize from 'serialize-javascript';
@@ -21,71 +21,71 @@ const renderTags = ({ tag: Tag, content, name, attribs }: ExtraTag) => (
   <Tag key={name} {...attribs} dangerouslySetInnerHTML={{ __html: content }} />
 );
 
-export class DocumentComponent extends PureComponent<DocumentInitialProps> {
-  static async getInitialProps({
-    assets,
-    data,
-    renderPage,
-    generateCriticalCSS = F,
-    extractor,
-    ...rest
-  }: Context): Promise<DocumentInitialProps> {
-    const page = await renderPage(data);
-    const criticalCSS = generateCriticalCSS();
-    return { assets, criticalCSS, data, extractor, ...rest, ...page };
-  }
-
-  render() {
-    const {
-      helmet,
-      assets,
-      criticalCSS,
-      data,
-      error,
-      errorComponent: ErrorComponent,
-      filterServerData = identity,
-      extractor,
-      extraHeadTags = [],
-      extraBodyTags = []
-    } = this.props;
-    // get attributes from React Helmet
-    const htmlAttrs = helmet.htmlAttributes.toComponent();
-    const bodyAttrs = helmet.bodyAttributes.toComponent();
-    const clientCss = path(['client', 'css'], assets);
-    return (
-      <html {...htmlAttrs}>
-        <head>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
-          />
-          {helmet.title.toComponent()}
-          {helmet.meta.toComponent()}
-          {helmet.link.toComponent()}
-          {helmet.script.toComponent()}
-          {extractor && getHeaderTags(extractor)}
-          {criticalCSS !== false && criticalCSS}
-          {clientCss && <link rel="stylesheet" href={clientCss} />}
-          {extraHeadTags.map(renderTags)}
-        </head>
-        <body {...bodyAttrs}>
-          <Root />
-          <Data data={filterServerData(data)} />
-          {error ? (
-            ErrorComponent ? (
-              <ErrorComponent error={error} />
-            ) : (
-              <Error message={error.message} stack={error.stack} />
-            )
-          ) : null}
-          {extraBodyTags.map(renderTags)}
-        </body>
-      </html>
-    );
-  }
+export function DocumentComponent({
+  helmet,
+  assets,
+  criticalCSS,
+  data,
+  error,
+  errorComponent: ErrorComponent,
+  filterServerData = identity,
+  extractor,
+  extraHeadTags = [],
+  extraBodyTags = []
+}: DocumentInitialProps) {
+  // get attributes from React Helmet
+  const htmlAttrs = helmet.htmlAttributes.toComponent();
+  const bodyAttrs = helmet.bodyAttributes.toComponent();
+  const clientCss = path(['client', 'css'], assets);
+  return (
+    <html {...htmlAttrs}>
+      <head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+        />
+        {helmet.title.toComponent()}
+        {helmet.meta.toComponent()}
+        {helmet.link.toComponent()}
+        {helmet.script.toComponent()}
+        {extractor && getHeaderTags(extractor)}
+        {criticalCSS !== false && criticalCSS}
+        {clientCss && <link rel="stylesheet" href={clientCss} />}
+        {extraHeadTags.map(renderTags)}
+      </head>
+      <body {...bodyAttrs}>
+        {error ? (
+          ErrorComponent ? (
+            <ErrorComponent error={error} />
+          ) : (
+            <Error message={error.message} stack={error.stack} />
+          )
+        ) : (
+          <Fragment>
+            <Root />
+            <Data data={filterServerData(data)} />
+          </Fragment>
+        )}
+        {extraBodyTags.map(renderTags)}
+      </body>
+    </html>
+  );
 }
+
+DocumentComponent.getInitialProps = async ({
+  assets,
+  data,
+  renderPage,
+  generateCriticalCSS = F,
+  extractor,
+  ...rest
+}: Context): Promise<DocumentInitialProps> => {
+  const page = await renderPage(data);
+  const criticalCSS = generateCriticalCSS();
+  return { assets, criticalCSS, data, extractor, ...rest, ...page };
+};
 
 export const Root = () => <div id="root">BEFORE.JS-DATA</div>;
 
