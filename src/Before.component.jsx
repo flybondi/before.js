1fb88de25b53ea1b5428638f50b60260e86e78ab
Luciano Fantone <luciano@flybondi.com>
2019-03-10 19:09:50 -0300

    fix: prevent multiple render on same page

--- src/Before.component.jsx
+++ src/Before.component.jsx
@@ -1,12 +1,15 @@
 // @flow strict
 import type {
   AsyncRoute,
+  BeforeAction,
   BeforeComponentWithRouterProps,
+  BeforeState,
   Context,
   InitialProps,
-  QueryType
+  QueryType,
+  ShouldRenderProps
 } from 'Before.component';
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useEffect, memo } from 'react';
 import { withRouter, Switch, Route, type ContextRouter } from 'react-router-dom';
 import { converge, find, nthArg, pipe, propEq } from 'ramda';
 import { isClientSide, getQueryString } from './utils';
@@ -18,7 +21,7 @@ import { isClientSide, getQueryString } from './utils';
  * @param {array} routes an array of route to filter
  * @returs {object|undefined} a valid route
  **/
-const getCurrentRouteByPath: (path: string, routes: Array<AsyncRoute>) => ?AsyncRoute = converge(
+const getRouteByPathname: (path: string, routes: Array<AsyncRoute>) => ?AsyncRoute = converge(
   find,
   [
     pipe(
@@ -50,26 +53,45 @@ const getPageProps = (
  * Retrieve the initial props from given component route.
  * @param {object} route react-router-v4 route object
  * @param {object} context object to pass into the getInitialProps
- * @param {function} setInitialProps react state hook
+ * @param {function} dispatch a useReducer dispatch function
  */
-const fetchInitialProps = async (
-  route: ?AsyncRoute,
-  context: Context,
-  setInitialProps: (props: { [key: string]: string }) => void
-) => {
+const fetchInitialProps = async (route: ?AsyncRoute, context: Context, dispatch) => {
+  const { location } = context;
+  const { pathname } = location;
   try {
     if (route) {
       const { component } = route;
       if (component && component.getInitialProps) {
-        const {
-          location: { pathname }
-        } = context;
         const data = await component.getInitialProps(context);
-        setInitialProps({ [pathname]: data });
+        dispatch({ type: 'end', location, props: { [pathname]: data } });
       }
     }
   } catch (error) {
-    setInitialProps({});
+    dispatch({ type: 'end', location, props: { [pathname]: null } });
+  }
+};
+
+/**
+ * Dispacher function to be use with `useReducer` hook.
+ * Manages the state of Before component
+ * @param {object} state Current state
+ * @param {object} action The dispatched action
+ * @return object
+ */
+const reducer = (state: BeforeState, { location, props, type }: BeforeAction) => {
+  switch (type) {
+    case 'start':
+      return { ...state, isFetching: true, nextLocation: location };
+    case 'end':
+      const { initialProps } = state;
+      return {
+        isFetching: false,
+        initialProps: { ...initialProps, ...props },
+        currentLocation: location,
+        nextLocation: null
+      };
+    default:
+      throw new Error('Invalid reducer type');
   }
 };
 
@@ -81,44 +103,28 @@ const fetchInitialProps = async (
 export function Before(props: BeforeComponentWithRouterProps) {
   const { data, routes, location, req } = props;
   const { pathname, key } = location;
-  const [currentLocation, setCurrentLocation] = useState(location);
-  const [isFetching, setIsFetching] = useState(false);
-  const [nextLocation, setNextLocation] = useState(null);
-  const [initialProps, setInitialProps] = useState({
-    [pathname]: data
+  const [state, dispatch] = useReducer(reducer, {
+    currentLocation: location,
+    isFetching: false,
+    nextLocation: null,
+    initialProps: { [pathname]: data }
   });
-  const getRoute = useCallback(getCurrentRouteByPath, [pathname, routes]);
+  const { isFetching, nextLocation, initialProps, currentLocation } = state;
 
   useEffect(() => {
-    isClientSide() && setIsFetching(true);
-  }, [key]);
-
-  useEffect(() => {
-    if (isFetching) {
-      setNextLocation(location);
-    }
-  }, [isFetching]);
+    isClientSide() && dispatch({ type: 'start', location });
+  }, [key, location]);
 
   useEffect(() => {
     if (isFetching && nextLocation) {
-      const route = getRoute(nextLocation.pathname, routes);
+      const route = getRouteByPathname(nextLocation.pathname, routes);
       fetchInitialProps(
         route,
         { ...props, location: nextLocation, query: getQueryString(nextLocation, req) },
-        data => setInitialProps({ ...initialProps, ...data })
+        dispatch
       );
     }
-  }, [nextLocation]);
-
-  useEffect(() => {
-    if (isFetching && nextLocation) {
-      setCurrentLocation(nextLocation);
-    }
-  }, [initialProps]);
-
-  useEffect(() => {
-    setIsFetching(false);
-  }, [currentLocation]);
+  }, [isFetching, nextLocation, props, req, routes]);
 
   const routeProps = initialProps[currentLocation.pathname];
   return (
@@ -130,7 +136,9 @@ export function Before(props: BeforeComponentWithRouterProps) {
             path={path}
             exact={exact}
             render={(context: ContextRouter) => (
-              <Component {...getPageProps(context, routeProps, req)} />
+              <ShouldRender isFetching={isFetching} location={context.location}>
+                <Component {...getPageProps(context, routeProps, req)} />
+              </ShouldRender>
             )}
           />
         );
@@ -138,5 +146,16 @@ export function Before(props: BeforeComponentWithRouterProps) {
     </Switch>
   );
 }
+
+/**
+ * Wrapper component that has a `shouldComponentUpdate` logic.
+ * Will only allow rendering on a location change and when the initial props were fetched.
+ */
+const ShouldRender = memo(
+  ({ children }: ShouldRenderProps) => children,
+  (prevProps: ShouldRenderProps, nextProps: ShouldRenderProps) => {
+    return !nextProps.isFetching && nextProps.location !== prevProps.location;
+  }
+);
 
 export default withRouter(Before);
