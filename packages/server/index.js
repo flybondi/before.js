c2d0d157141c38f76f495f7795b10f86918853bd
Luciano Fantone <luciano@flybondi.com>
2019-05-28 09:01:54 -0300

    fix: expose only the render method

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,6 +1,3 @@
 // @flow strict;
 
-export { Error } from './src/Error.component';
-export { DocumentComponent, Root, Data } from './src/Document.component';
-export * from './src/fetchInitialPropsFromRoute';
-export * from './src/render';
+export { render } from './src/render';
