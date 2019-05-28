e9733a087a43760d65e3ec3a140119119c0d9dd6
Luciano Fantone <luciano@flybondi.com>
2019-05-28 16:01:08 -0300

    chore: expose unsure ready method

--- packages/client/index.js
+++ packages/client/index.js
@@ -1,5 +1,5 @@
 // @flow strict;
 
 export { default as Before } from './src/Before.component';
-export * from './src/Async.component';
-export * from './src/ensureReady';
+export { asyncComponent } from './src/Async.component';
+export { ensureReady } from './src/ensureReady';
